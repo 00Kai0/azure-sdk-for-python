148476f1b4cc8dc0fdbb4221776a94c23bf68fac
00Kai0 <sunkaihuisos@gmail.com>
2020-03-23 13:16:50 +0800

    update test for resource links

--- sdk2/resources/azure-mgmt-resource/tests/test_mgmt_resource_locks.py
+++ sdk2/resources/azure-mgmt-resource/tests/test_mgmt_resource_locks.py
@@ -5,9 +5,15 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 #--------------------------------------------------------------------------
+
+# coverd ops:
+#   management_locks: /16
+#   authorization_operations: 1/1
+
 import unittest
 
 import azure.mgmt.resource
+import azure.mgmt.resource.resources.v2019_07_01
 from devtools_testutils import AzureMgmtTestCase, ResourceGroupPreparer
 
 class MgmtResourceLocksTest(AzureMgmtTestCase):
@@ -18,8 +24,85 @@ class MgmtResourceLocksTest(AzureMgmtTestCase):
             azure.mgmt.resource.ManagementLockClient
         )
 
+        self.resource_client_v07 = self.create_mgmt_client(
+            azure.mgmt.resource.resources.v2019_07_01.ResourceManagementClient
+        )
+
+    @unittest.skip("Forbidden")
+    def test_locks_at_subscription_level(self):
+        pass
+
+    @unittest.skip("Forbidden")
+    def test_locks_by_scope(self):
+        pass
+
+    @unittest.skip("Forbidden")
     @ResourceGroupPreparer()
-    def test_locks(self, resource_group, location):
+    def test_locks_at_resource_level(self, resource_group, location):
+        lock_name = 'pylockrg'
+        resource_name = self.get_resource_name("pytestavset")
+
+        # create resource
+        create_result = self.resource_client_v07.resources.begin_create_or_update(
+            resource_group_name=resource_group.name,
+            resource_provider_namespace="Microsoft.Compute",
+            parent_resource_path="",
+            resource_type="availabilitySets",
+            resource_name=resource_name,
+            parameters={'location': self.region},
+            # api_version="2019-07-01"
+        )
+
+        lock = self.locks_client.management_locks.create_or_update_at_resource_level(
+            resource_group_name=resource_group.name,
+            resource_provider_namespace="Microsoft.Compute",
+            parent_resource_path="",
+            resource_type="availabilitySets",
+            resource_name=resource_name,
+            lock_name=lock_name,
+            parameters={
+                'level': 'CanNotDelete'
+            }
+        )
+        self.assertIsNotNone(lock)
+
+        self.locks_client.management_locks.get_at_resource_group_level(
+            resource_group_name=resource_group.name,
+            resource_provider_namespace="Microsoft.Compute",
+            parent_resource_path="",
+            resource_type="availabilitySets",
+            resource_name=resource_name,
+            lock_name=lock_name,
+        )
+
+        locks = list(self.locks_client.management_locks.list_at_resource_group_level(
+            resource_group_name=resource_group.name,
+            resource_provider_namespace="Microsoft.Compute",
+            parent_resource_path="",
+            resource_type="availabilitySets",
+            resource_name=resource_name,
+        ))
+        self.assertEqual(len(locks), 1)
+
+        lock = self.locks_client.management_locks.delete_at_resource_group_level(
+            resource_group_name=resource_group.name,
+            resource_provider_namespace="Microsoft.Compute",
+            parent_resource_path="",
+            resource_type="availabilitySets",
+            resource_name=resource_name,
+            lock_name=lock_name,
+        )
+
+        # delete resource
+        async_delete = self.resource_client_v07.resource_groups.begin_delete(
+            resource_group.name
+        )
+        async_delete.wait()
+
+
+    @unittest.skip("Forbidden")
+    @ResourceGroupPreparer()
+    def test_locks_at_resource_group_level(self, resource_group, location):
         lock_name = 'pylockrg'
 
         lock = self.locks_client.management_locks.create_or_update_at_resource_group_level(
@@ -31,6 +114,11 @@ class MgmtResourceLocksTest(AzureMgmtTestCase):
         )
         self.assertIsNotNone(lock)
 
+        self.locks_client.management_locks.get_at_resource_group_level(
+            resource_group.name,
+            lock_name
+        )
+
         locks = list(self.locks_client.management_locks.list_at_resource_group_level(
             resource_group.name
         ))
@@ -40,6 +128,9 @@ class MgmtResourceLocksTest(AzureMgmtTestCase):
             resource_group.name,
             lock_name
         )
+
+    def test_operations(self):
+        self.locks_client.authorization_operations.list()
 
 
 #------------------------------------------------------------------------------
